69e7cccf51b9bc9940ba231394088f858779e99b
BrianQcq <qiuchuanqin@gmail.com>
2019-08-12 14:56:30 -0700

    bug fixed

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,30 @@
+// import React from "react";
+// import { withRouter } from "react-router-dom";
+// import "./menu-item.styles.scss";
+
+// const MenuItem = ({ title, imageUrl, size, linkeUrl, history, match }) => (
+//   <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkeUrl}`)}>
+//     <div
+//       className="background-image"
+//       style={{
+//         backgroundImage: `url(${imageUrl})`
+//       }}
+//     />
+//     <div className="content">
+//       <h1 className="title">{title.toUpperCase()}</h1>
+//       <span className="subtitle">SHOP NOW</span>
+//     </div>
+//   </div>
+// );
+
+// export default withRouter(MenuItem);
+
 import React from "react";
 import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size, linkeUrl, history, match }) => (
-  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkeUrl}`)}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className="background-image"
       style={{
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
@@ -7,7 +8,6 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
   const { title, items } = collection;
   return (
     <div className="collection-page">
